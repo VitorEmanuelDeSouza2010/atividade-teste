93e7a41a3c24aa9c051742b9b468fb94a3bc1933
Vitor Emanuel de Souza <vitorlego321@gmail.com>
2025-11-28 10:37:40 -0300

    Add files via upload

---
+++ src/components/card.jsx
@@ -0,0 +1,21 @@
+import s from './card.module.css'
+
+//export const Card = () => {
+//    return(
+//        <>
+//            <img src={props.image} alt={props.name} />
+//            <h2>{props.name}</h2>
+//            <p>{props.species}</p>
+//       </>
+//    )
+// }
+
+export const Card = ({image, name, species}) => {
+    return(
+        <>
+            <img src={image} alt={name} />
+            <h2>{name}</h2>
+            <p>{species}</p>
+        </>
+    )
+}
