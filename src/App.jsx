280c090241b886515f9b7befb86edbf641951192
Vitor Emanuel de Souza <vitorlego321@gmail.com>
2025-12-05 11:21:29 -0300

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -21,7 +21,7 @@ function App() {
 
   return (
     <>
-      {modal !== undefined && <infoModal data={data[modal]} close={() => setModal()}/>}
+      {modal !== undefined && <InfoModal data={data[modal]} close={() => setModal()}/>}
       <h1 className={s.title}>Rick and Morty Characters</h1>
       <main>
         <div style={{display: "flex", gap: "10px", alignItems: "center", justifyContent: "center"}}>
