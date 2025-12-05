d2d8e4633188f7898ae6c354200273264c2043c7
Vitor Emanuel de Souza <vitorlego321@gmail.com>
2025-12-05 10:56:20 -0300

    Add files via upload

---
+++ src/components/infoModal.jsx
@@ -0,0 +1,25 @@
+import s from "./infoModal.module.css"
+
+const InfoModal = ({data, close}) => {
+    return(
+        <div className={s.wrapInfo}>
+            <button onClick={close}>❌</button>
+            <div>
+                <img src={data.image} alt={data.name} />
+                <h2> <strong>Name: </strong>{data.name}</h2>
+                <p> <strong>Status: </strong> {data.status}</p>
+                <p> <strong>Origin: </strong> {data.origin.name}</p>
+                <p> <strong>Species: </strong> {data.species}</p>
+                <p> <strong>Gender: </strong> {data.gender}</p>
+                <p> <strong>Location: </strong> {data.location.name}</p>
+                <p> <strong>Created: </strong> {new Date(data.created).toLocaleTimeString("pt-BR",{
+                    day: "2-digit",
+                    month: "2-digit",
+                    year: "numeric"
+                })}</p>
+            </div>
+        </div>
+    )
+}
+
+export default InfoModal
